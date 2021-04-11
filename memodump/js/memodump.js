2b6a847ff32b2f4bb393e439025dc64cf3439271
georgschweflinghaus <gejosh(at)schweflinghaus.de>
2021-04-11 12:50:48 +0200

    Re-style the Edit Save button

---
+++ memodump/js/memodump.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2021.
+ * Licensed under GPL.
+ */
+
+if (typeof jQuery === 'undefined') { throw new Error('jQuery is required for toggle.js!') }
+if ($.fn.emulateTransitionEnd === undefined) { throw new Error('Bootstrap is required for toggle.js!') }
+
++function ($) {
+  /* Re-style the standard MoinMoin Page Edit Save button */
+  $('input[name=button_save]').removeClass('button').addClass('btn').addClass('btn-primary');
+}(jQuery);
