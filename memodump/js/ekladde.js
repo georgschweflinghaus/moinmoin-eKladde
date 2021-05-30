3f87129e5bc359ea9d87b3422e0d75a5bc6d8ed4
georgschweflinghaus <gejosh(at)schweflinghaus.de>
2021-05-31 00:38:15 +0200

    Changed order of elements in edit mode. Changed button appearance in edit mode

--- memodump/js/ekladde.js
+++ memodump/js/ekladde.js
@@ -3,9 +3,26 @@
  * Licensed under GPL.
  */
 
-if (typeof jQuery === 'undefined') { throw new Error('jQuery is required for toggle.js!') }
+if (typeof jQuery === 'undefined') { throw new Error('jQuery is required') }
 
 +function ($) {
   /* Re-style the standard MoinMoin Page Edit Save button */
   $('input[name=button_save]').removeClass('button').addClass('btn').addClass('btn-primary');
+  $('input[name=button_cancel]').removeClass('button').addClass('btn').addClass('btn-outline-danger');
+  $('input[name=button_preview]').removeClass('button').addClass('btn').addClass('btn-outline-secondary');
+  $('input[name=button_spellcheck]').removeClass('button').addClass('btn').addClass('btn-outline-secondary');
+  $('input[name=button_switch]').removeClass('button').addClass('btn').addClass('btn-outline-secondary');
+
+}(jQuery);
+
+// Edit view has usualy the order:
+// 1. Textedit
+// 2. Editor Help
+// 3. Preview
+// but we need
+// 1. Textedit
+// 2. Preview
+// 3. Editor Help
++function ($) {
+  $('#previewbelow').insertBefore('#editor-help');
 }(jQuery);
